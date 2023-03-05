31dc7414410cb019254038926be7286eb4cd7fd0
Marastana3 <smaramaria@ymail.com>
2023-03-05 19:21:30 +0200

    ada lab 2

--- ada/BST.c
+++ ada/BST.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node {
+#include <stdbool.h>
+struct node{
  int key;
  struct node* left;
  struct node* right;
@@ -10,7 +11,7 @@ struct node {
 typedef struct node* Node;
 
 /* Create a new node */
-Node newNode(int key) {
+Node newNode(int key){
  Node node = (Node) malloc(sizeof(struct node));
  node->key = key;
  node->left = NULL;
@@ -44,8 +45,9 @@ void insert(Node* root, Node z) {
 
 }
 
-
-void inorder(Node root) {
+/* Inorder function, prints the binary search tree as follows: 
+   ROOT --> LEFT --> RIGHT*/
+void inorder(Node root){
     if(root != NULL){
         inorder(root->left);
         printf("%d ", root->key);
@@ -53,7 +55,18 @@ void inorder(Node root) {
     }
 }
 
-int height(Node root) {
+/* Postorder function, traverses the binary search tree as follows:
+   the subtrees from LEFT --> subtrees from RIGHT --> ROOT  */
+void postorder(Node root){
+    if(root != NULL){
+        postorder(root->left);
+        postorder(root->right);
+         printf("%d", root->key);
+    }
+}
+
+/*The height of a BST is represented by the longest path from ROOT --> LEAF NODE*/
+int height(Node root){
     if(root == NULL)
         return 0;
     else{
@@ -68,8 +81,8 @@ int height(Node root) {
     }
 }
 
-
-int search(Node root, int key) {
+/* Returns the node containing the key or NULL if the key is not present*/
+int search(Node root, int key){
     while(root != NULL && key != root->key){
         if(key < root->key)
             root = root->left;
@@ -79,9 +92,102 @@ int search(Node root, int key) {
     return root->key;
 }
 
+/*Helper function (find minimum) for the successor function*/
+int treeMinimum(Node root){
+    while(root->left != NULL){
+        root = root->left;
+    }
+    return root->key;
+}
 
-int main()
-{
+/*Returns the node containing the sucessor of node or NULL if there is no 
+successor of node*/
+int successor(Node root, int key){
+    Node y;
+    if(root->right != NULL){
+        return treeMinimum(root->right);
+   }
+   y = root->p;
+   while(y != NULL && root == y->right){
+        root = y;
+        y = y->p;
+   }
+   return y->key;
+}
+
+/*Helper function for the IsPerfectlyBalanced function*/
+int countNodes(Node* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+
+/*IsPerfectlyBalanced returns true if BST is perfectly balanced*/
+bool IsPerfectlyBalanced(Node* root) {
+    if (root == NULL) {
+        return true;
+    }
+    int left_count = countNodes(root->left);
+    int right_count = countNodes(root->right);
+    return abs(left_count - right_count) <= 1 && IsPerfectlyBalanced(root->left) && IsPerfectlyBalanced(root->right);
+}
+
+/*SearchClosest function that finds the node containing the key 
+with the value closest to k*/
+Node* SearchClosest(Node* root, int k) {
+    Node* closest = root;
+    while (root != NULL) {
+        if (abs(root->key - k) < abs(closest->key - k)) {
+            closest = root;
+        }
+        if (root->key == k) {
+            return root;
+        }
+        else if (root->key < k) {
+            root = root->right;
+        }
+        else {
+            root = root->left;
+        }
+    }
+    return closest;
+}
+
+/*CheckExistTwoNodesWithSum: function to check if there are 2 nodes whose 
+sum is equal with s*/
+bool CheckExistTwoNodesWithSum(Node* root, int s) {
+    int arr[1000];
+    int len = 0;
+    bool flag = false;
+
+    void Inorder(Node* root) {
+        if (root == NULL) {
+            return;
+        }
+        Inorder(root->left);
+        arr[len++] = root->key;
+        Inorder(root->right);
+    }
+    Inorder(root);
+    int i = 0;
+    int j = len - 1;
+    while (i < j) {
+        if (arr[i] + arr[j] == s) {
+            flag = true;
+            break;
+        }
+        else if (arr[i] + arr[j] < s) {
+            i++;
+        }
+        else {
+            j--;
+        }
+    }
+    return flag;
+}
+
+int main(void){
     Node tree=NULL;
 
     insert(&tree,newNode(7));
@@ -98,6 +204,8 @@ int main()
 
     int found = search(tree, 10);
 
-    printf("height: %d, found: %d\n", h, found);
+    int success = successor(tree, 3);
+
+    printf("height: %d, found: %d, success: %d\n", h, found, success);
 
 }

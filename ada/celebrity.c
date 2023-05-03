7eed51d1334f76cd533d86d15539fd4d8e96ee35
Marastana3 <smaramaria@ymail.com>
2023-05-03 17:05:26 +0300

    adjustments

--- ada/celebrity.c
+++ ada/celebrity.c
@@ -17,9 +17,9 @@ bool knows(int a, int b) {
 
 // Returns the id of the celebrity if there is one, -1 otherwise
 int findCelebrity() {
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; ++i) {
         bool isCelebrity = true;
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j < N; ++j) {
             // If i knows j or j doesn't know i, i can't be a celebrity
             if (i != j && (knows(i, j) || !knows(j, i))) {
                 isCelebrity = false;
--- ada/primMST.c
+++ ada/primMST.c
@@ -7,7 +7,7 @@
 //helper function that returns the vertex with the minimum key value that is not yet in the MST
 int minKey(int key[], bool mstSet[]) {
     int min = INT_MAX, min_index;
-    for (int v = 0; v < MAX_LEN; v++)
+    for (int v = 0; v < MAX_LEN; ++v)
         if (mstSet[v] == false && key[v] < min)
             min = key[v], min_index = v;
     return min_index;
@@ -15,22 +15,22 @@ int minKey(int key[], bool mstSet[]) {
 //helper function that prints the edges in the MST
 void printMST(int parent[], int graph[MAX_LEN][MAX_LEN]) {
     printf("Edge \tWeight\n");
-    for (int i = 1; i < MAX_LEN; i++)
-        printf("%d - %d \t%d \n", parent[i], i, graph[i][parent[i]]);
+    for (int i = 1; i < MAX_LEN; ++i)
+        printf("%d - %d \t%d \n", parent[i] + 1, i + 1, graph[i][parent[i]]);
 }
 
 void primMST(int graph[MAX_LEN][MAX_LEN]) {
     int parent[MAX_LEN];
     int key[MAX_LEN];
     bool mstSet[MAX_LEN];
-    for (int i = 0; i < MAX_LEN; i++)
+    for (int i = 0; i < MAX_LEN; ++i)
         key[i] = INT_MAX, mstSet[i] = false;
     key[0] = 0;
     parent[0] = -1;
-    for (int count = 0; count < MAX_LEN - 1; count++) {
+    for (int i = 0; i < MAX_LEN - 1; ++i) {
         int u = minKey(key, mstSet);
         mstSet[u] = true;
-        for (int v = 1; v < MAX_LEN; v++)
+        for (int v = 1; v < MAX_LEN; ++v)
             if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
                 parent[v] = u, key[v] = graph[u][v];
     }
